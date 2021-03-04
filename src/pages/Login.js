ac9e507981a5bbfff49752e885ed3739aba9e9ed
Emil <Emilknudsen1@live.dk>
2021-03-04 12:25:31 +0100

    callback

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,20 @@
 import querystring from "querystring";
 import LoginCard from "../components/LoginCard";
 
+var uri;
+
+if (window.location.href.includes("netlify")) {
+	uri = "https://iplaymusic-emilk.netlify.app/callback";
+} else {
+	uri = "http://localhost:8888/callback";
+}
+
 export default function Login() {
 	var queryParameters = querystring.stringify({
 		response_type: "code",
 		client_id: "08daff713cef430b86db5e9db1a30b1b",
 		scope: "user-read-private user-read-email",
-		redirect_uri: "http://localhost:8888/callback",
+		redirect_uri: uri,
 		state: "asdasdasdakosadfsdafasdfdsfadsasdasdfasd",
 	});
 	return (
